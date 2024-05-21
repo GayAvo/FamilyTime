588ee873fa4ed2b36d3f12023af597ae73b42e53
GayAvo <weelosophy@gmail.com>
2024-05-21 10:02:24 +0000

    DropdownAge

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-var username1 = "lucy"
+// var username1 = "lucy"
 
-console.log (username1)
+// console.log (username1)
 
-var username1age = 20
+// var username1age = 20
 
-console.log (username1age)
+// console.log (username1age)
 
+function activities() {
+    var user_input = document.getElementById("Age").value
+
+    if (user_input == "5 and below") {
+        document.getElementById("age-response").textContent = "The Artground – A Curious Place to be is managed by The Ground Co Limited, a registered arts charity dedicated to children from birth to 9 years old. As the first multi-disciplinary children’s arts centre in Singapore, we aim to provide positive arts experiences across a variety of art forms, and build bridges between artists, educators, parents and children from various communities so that they may discover and co-create meaningful arts experiences together. https://www.theartground.com.sg/"
+    } else if (user_input == "6 - 10 years") {
+        document.getElementById("age-response").textContent = "The Singapore Botanic Gardens (the Gardens) was inscribed as a UNESCO World Heritage Site on 4th July 2015, at the 39th session of the World Heritage Committee (WHC) in Bonn, Germany. The Gardens is the first and only tropical botanic garden on the UNESCO’s World Heritage List. https://www.nparks.gov.sg/sbg/whats-happening/calendar-of-events"
+    } else {
+        document.getElementById("age-response").textContent = "National Gallery Singapore is a leading visual arts institution which oversees the world’s largest public collection of Singapore and Southeast Asian modern art. The Gallery aims to be a progressive museum that creates dialogues between the art of Singapore, Southeast Asia and the world to foster and inspire a creative and inclusive society. https://www.nationalgallery.sg/whats-on"
+    }
+
+
+    // Get the value from the input field of ID #user-input
+    // Store this value in a variable called user_input
+    // var user_input = document.getElementById("user-input").value
+
+    // Update the content for the HTML element with ID #header-age with 
+    // whatever the user_input varible contains
+    // document.getElementById("header-age").textContent = user_input
+
+}
